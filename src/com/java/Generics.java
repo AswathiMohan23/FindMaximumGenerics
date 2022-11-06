6d6c4cc5714dc9a182bc318a23e8dc5ddfa09c41
AswathiMohan23 <achu999here@gmail.com>
2022-11-06 09:06:44 +0530

    [ADDED] : code to pass more than 3 paramters into the method and added code to sort the data in ascending order

--- src/com/java/Generics.java
+++ src/com/java/Generics.java
@@ -1,25 +1,34 @@
 package com.java;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Generics <T extends Comparable<T>> {
     T firstValue;
     T secondValue;
     T thirdValue;
+    T fourthValue;
 
-    public void Generics(T firstValue, T secondValue, T thirdValue) {
+    public void Generics(T firstValue, T secondValue, T thirdValue, T fourthValue) {
         this.firstValue = firstValue;
         this.secondValue = secondValue;
         this.thirdValue = thirdValue;
+        this.fourthValue=fourthValue;
 
     }
 
 
-    public <T extends Comparable> Object comparing(T firstValue, T secondValue, T thirdValue) {
-        if ((thirdValue.compareTo(secondValue) > 0) && (thirdValue.compareTo(firstValue) > 0))
-            System.out.println(thirdValue + " greater than " + secondValue + " and " + firstValue);
-        else if ((secondValue.compareTo(firstValue) > 0) && (secondValue.compareTo(thirdValue) > 0))
-            System.out.println(secondValue + " greater than " + firstValue + " and " + thirdValue);
-        else if ((firstValue.compareTo(secondValue) > 0) && (firstValue.compareTo(thirdValue) > 0))
-            System.out.println(firstValue + " greater than " + secondValue + " and " + thirdValue);
+    public <T extends Comparable> Object comparing(T firstValue, T secondValue, T thirdValue,T fourthValue) {
+        ArrayList <T>arrayList=new ArrayList<T>();
+        arrayList.add(firstValue);
+        arrayList.add(secondValue);
+        arrayList.add(thirdValue);
+        arrayList.add(fourthValue);
+        System.out.println(arrayList);
+        Collections.sort(arrayList);
+        System.out.println("sorted list : "+arrayList);
+
         return null;
     }
 
--- src/com/java/MaximumMain.java
+++ src/com/java/MaximumMain.java
@@ -11,22 +11,23 @@ public class MaximumMain<T> {
     static Generics<Integer> object = new Generics<>();
 
     public static void findingMaxInteger() {
-        System.out.println("\nFinding max integer.....................");
+        System.out.println("\ninteger Sorted in ascending order.....................");
         //object number1 = 10, number2 = 30, number3 = 200;
-        object.comparing(10, 30, 200);
+        object.comparing(10, 30, 200,300);
     }
 
     public static void findingMaxFloat() {
-        System.out.println("\nFinding max Float.....................");
+        System.out.println("\nFloat Sorted in ascending order.....................");
         Float number1 = Float.valueOf(10);
         Float number2 = Float.valueOf(80);
         Float number3 = Float.valueOf(2);
-        object.comparing(number1, number2, number3);
+        Float number4 = Float.valueOf(90);
+        object.comparing(number1, number2, number3,number4);
     }
     public static void findingMaxString() {
-        System.out.println("\nFinding the longest string.....................");
-        String firstValue = "banana", secondValue = "watermelon", thirdValue = "apple";
-        object.comparing(firstValue, secondValue, thirdValue);
+        System.out.println("\nSorted in ascending order.....................");
+        String firstValue = "banana", secondValue = "watermelon", thirdValue = "apple",fourthValue="plum";
+        object.comparing(firstValue, secondValue, thirdValue,fourthValue);
     }
 
 }
